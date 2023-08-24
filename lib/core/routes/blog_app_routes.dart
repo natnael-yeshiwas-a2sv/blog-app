011a136611904f7fc8499dc2fe60a09568ccb166
Natnael Yeshiwas <natiyeshi16@gmail.com>
2023-08-24 09:57:52 +0300

    Initial commit

---
+++ lib/core/routes/blog_app_routes.dart
@@ -0,0 +1,10 @@
+class BlogAppRoutes {
+  static const HOME = '/';
+  static const ARTICLES = '/articles';
+  static const ARTICLE_DETAIL = '/article-detail';
+  static const ARTICLE_CREATE = '/article-create';
+  static const ARTICLE_EDIT = '/article-edit';
+  static const AUTH = '/auth';
+  static const PROFILE = '/profile';
+  static const ONBOARDING = '/on-boarding';
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:blog_application/features/blog/presentation/pages/articles_reading_screen.dart';
+import "./Route.dart" as route;
+import "core/routes/blog_app_routes.dart";
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    onGenerateRoute: route.controller,
+    initialRoute: BlogAppRoutes.ARTICLE_CREATE,
+  ));
+}
