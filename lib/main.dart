8d5abd62ae047b6b86749c12b6806d4bf9345758
yidiyetebeje <yidnekachew.tebeje@a2sv.org>
2023-08-24 10:51:13 +0300

    feat(mobile): setup dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:blog_application/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:blog_application/features/blog/presentation/pages/articles_reading_screen.dart';
 import "./Route.dart" as route;
 import "core/routes/blog_app_routes.dart";
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setup();
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     onGenerateRoute: route.controller,
