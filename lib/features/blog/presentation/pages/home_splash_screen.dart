06424e3842d411e67bb1230a1ba0e5dbab4cbcb3
yakob-solomon-a2sv <yakob.solomon@a2sv.org>
2023-08-29 10:45:16 +0300

    practice

--- lib/features/blog/presentation/pages/home_splash_screen.dart
+++ lib/features/blog/presentation/pages/home_splash_screen.dart
@@ -4,6 +4,7 @@ import 'package:blog_application/features/blog/presentation/pages/onboarding_pag
 import 'package:blog_application/features/blog/domain/usecases/isloged_in_usecase.dart';
 import 'package:blog_application/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:blog_application/features/blog/presentation/pages/home_page.dart';
 
 class home extends StatefulWidget {
   const home({super.key});
@@ -13,22 +14,29 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(milliseconds: 5000), () {
+      Navigator.pushReplacementNamed(context, BlogAppRoutes.ONBOARDING);
+    });
+  }
+
+  // Widget changeScreen()  {
+  //   IsLogedIn login = sl<IsLogedIn>();
+  //   bool islogedin =  login();
+  //   return islogedin ? HomePage() : const Onboarding();
+  // }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)  {
     return Scaffold(
       body: AnimatedSplashScreen(
         splash: Image.asset("assets/images/a2sv.png"),
-        nextScreen: Onboarding(),
+        nextScreen: Container(),
         splashTransition: SplashTransition.scaleTransition,
-        duration: 2000,  // Duration in milliseconds
- 
+        duration: 2000, // Duration in milliseconds
       ),
     );
-    
-    
-    
-    
-    
   }
 }
