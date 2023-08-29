80e0c339445a96cae608db9ed7584c8062163bb9
yakob-solomon-a2sv <yakob.solomon@a2sv.org>
2023-08-29 10:09:35 +0300

    fix(mobile): updating the splash screen

--- lib/features/blog/presentation/pages/home_splash_screen.dart
+++ lib/features/blog/presentation/pages/home_splash_screen.dart
@@ -1,4 +1,6 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:blog_application/core/routes/blog_app_routes.dart';
+import 'package:blog_application/features/blog/presentation/pages/onboarding_page.dart';
 import 'package:blog_application/features/blog/domain/usecases/isloged_in_usecase.dart';
 import 'package:blog_application/service_locator.dart';
 import 'package:flutter/material.dart';
@@ -11,41 +13,22 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
-  @override
-  void initState() {
-    super.initState();
-//
-
-    Future.delayed(Duration(seconds: 5),  () async {
-      IsLogedIn loggedInst = sl<IsLogedIn>();
-      bool islogedin = await loggedInst();
-      String route = islogedin ? BlogAppRoutes.HOME : BlogAppRoutes.ONBOARDING;
-      Navigator.popAndPushNamed(context, route);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 244, 243, 244),
-      body: Stack(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        // crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
-              width: 233,
-              height: 94,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/a2sv.png"),
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-          ),
-        ],
+      body: AnimatedSplashScreen(
+        splash: Image.asset("assets/images/a2sv.png"),
+        nextScreen: Onboarding(),
+        splashTransition: SplashTransition.scaleTransition,
+        duration: 2000,  // Duration in milliseconds
+ 
       ),
     );
+    
+    
+    
+    
+    
   }
 }
