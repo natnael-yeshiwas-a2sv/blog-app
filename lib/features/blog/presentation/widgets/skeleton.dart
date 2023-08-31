2fde032bec7fc7b9c1289b085c5615d481a4e3a2
Natnael Yeshiwas <natiyeshi16@gmail.com>
2023-08-31 11:43:34 +0300

    add animation

--- lib/features/blog/presentation/widgets/skeleton.dart
+++ lib/features/blog/presentation/widgets/skeleton.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animator/animator.dart';
 
 class Skeleton extends StatelessWidget {
   const Skeleton({Key? key, this.height, this.width}) : super(key: key);
@@ -12,15 +13,28 @@ class Skeleton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height,
-      width: width,
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-          color: isDarkMode(context)
-              ? Colors.black.withBlue(2)
-              : Colors.black.withOpacity(0.04),
-          borderRadius: const BorderRadius.all(Radius.circular(16))),
+    return Animator(
+      tween: Tween<double>(begin: .5, end: 1),
+      cycles: 0,
+      repeats: 0,
+      duration: Duration(milliseconds: 500),
+      builder: (BuildContext context, AnimatorState<double> animatorState,
+          Widget? child) {
+        return Opacity(
+          opacity: animatorState.value,
+          child: Container(
+            height: height,
+            width: width,
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                color: isDarkMode(context)
+                    ? Colors.black.withBlue(2)
+                    : Colors.black.withOpacity(0.1),
+                borderRadius: const BorderRadius.all(Radius.circular(16))),
+          ),
+        );
+      },
+      // child:
     );
   }
 }
